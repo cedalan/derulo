025a141804b2577193ef1f8716c3f43ad91c2ec6
Elias Dalan <ceadyy@gmail.com>
2025-05-24 22:44:39 +0200

    Added utils

---
+++ core/utils.py
@@ -0,0 +1,16 @@
+#Different utils functions
+
+def check_for_duplicates():
+    """
+    Checks if there are duplicates in the data provided. I. ex. if response is an array of the same objects:
+    0:
+        user_id: 123
+        firstname: "Ola"
+        lastname: "Nordmann"
+        email: "Ola@Nordmann.no"
+    1:
+        user_id: 456
+        firstname: "Kari"
+        lastname: "Nordmann"
+        email: "Kari@Nordmann.no"
+    """
